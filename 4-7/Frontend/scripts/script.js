b98bbe7e79d6c0417aaf3db14bc71081b4fa9c3b
vitorrf4 <vitorrf4@gmail.com>
2023-11-08 23:33:39 -0300

    create new task with form

--- 4-7/Frontend/scripts/script.js
+++ 4-7/Frontend/scripts/script.js
@@ -1,6 +1,5 @@
 async function getAllTasks() {
-    const response = await fetch("http://localhost:8080/tasks", {
-    });
+    const response = await fetch("http://localhost:8080/tasks",);
     const json = await response.json();
 
     console.log(json);
@@ -10,10 +9,34 @@ async function getAllTasks() {
 
 function createListFromTasks(jsonTaskList) {
     const ul = document.getElementById("task_list");
+    ul.innerHTML = "";
 
     for (let task of jsonTaskList) {
         const li = document.createElement("li");
-        li.innerHTML = `Title: ${task.title}`;
+        li.innerHTML = `Title: ${task.title} | Description ${task.description}`;
         ul.append(li);
+    }
+}
+
+function insertTaskIntoList(task) {
+    const ul = document.getElementById("task_list");
+    const li = document.createElement("li");
+
+    li.innerHTML = `Title: ${task.title} | Description ${task.description}`;
+    ul.append(li);
+}
+
+async function saveTask(title, description) {
+    const taskJson = JSON.stringify({title: title.value, description: description.value});
+    console.log(taskJson);
+
+    const response = await fetch("http://localhost:8080/tasks", {
+        method: 'post',
+        body: taskJson,
+        headers : {"Content-Type": "application/json"}
+    });
+    if (response.status === 201) {
+        const taskJson = await response.json()
+        insertTaskIntoList(taskJson);
     }
 }
