f146c32d8a537b88361bcfd5e75856a3a0f4a355
vitorrf4 <vitorrf4@gmail.com>
2023-11-09 13:40:59 -0300

    change mark as complete button to change status

--- 4-7/Frontend/scripts/script.js
+++ 4-7/Frontend/scripts/script.js
@@ -1,10 +1,21 @@
+let taskListDiv;
+let tasksUl;
+let currentPage = '';
+
+function init() {
+    taskListDiv = document.getElementById("task_list");
+    tasksUl = document.getElementById("task_ul");
+}
+
 async function renderTasks(taskStatus) {
     let response;
 
     if (!taskStatus) {
         response = await fetch("http://localhost:8080/tasks");
+        currentPage = '';
     } else {
         response = await fetch(`http://localhost:8080/tasks/status/${taskStatus}`);
+        currentPage = taskStatus;
     }
     const jsonResponse = await response.json();
 
@@ -12,7 +23,7 @@ async function renderTasks(taskStatus) {
 }
 
 function createListFromTasks(jsonTaskList) {
-    document.getElementById("task_list").innerHTML = "";
+    tasksUl.innerHTML = "";
 
     for (let task of jsonTaskList) {
         insertTaskIntoList(task);
@@ -20,14 +31,14 @@ function createListFromTasks(jsonTaskList) {
 }
 
 function insertTaskIntoList(task) {
-    const ul = document.getElementById("task_list");
     let li = document.createElement("li");
 
-    li.innerHTML = `Title: ${task.title} | Description ${task.description} | Status: ${task.taskStatus}`;
+    li.innerHTML = `Title: ${task.title} | Description: ${task.description} | Status: ${task.taskStatus}`;
     li.id = task.id;
     li = createButtons(li, task);
 
-    ul.append(li);
+    tasksUl.appendChild(li);
+    taskListDiv.append(tasksUl);
 }
 
 function createButtons(li, task) {
@@ -35,37 +46,41 @@ function createButtons(li, task) {
     deleteButton.innerHTML = "Delete";
     deleteButton.onclick = () => deleteTask(task.id);
 
-    const markAsCompleteButton = document.createElement("button");
-    markAsCompleteButton.innerHTML = "Mark as Completed";
-    markAsCompleteButton.onclick = () => markAsComplete(task.id);
+    const changeStatusButton = document.createElement("button");
+    changeStatusButton.innerHTML = "Change status";
+    changeStatusButton.onclick = () => changeStatus(task);
 
-    li.append(deleteButton, markAsCompleteButton);
+    li.append(deleteButton, changeStatusButton);
     return li;
 }
 
-async function markAsComplete(taskId) {
-    console.log("task id: " + taskId);
-    await fetch(`http://localhost:8080/tasks/${taskId}/completed`, {
+async function changeStatus(task) {
+    let newStatus;
+
+    if (task.taskStatus === 'COMPLETED') {
+        newStatus = 'PENDING';
+    } else {
+        newStatus = 'COMPLETED';
+    }
+
+    await fetch(`http://localhost:8080/tasks/${task.id}/${newStatus}`, {
         method: 'put'
     })
         .then(() => {
-            console.log(`task ${taskId} completed`);
-            renderTasks();
+            renderTasks(currentPage);
         })
         .catch(err => {
             console.log("Error: ");
             console.log(err);
         });
-
 }
 
 async function deleteTask(taskId) {
     await fetch(`http://localhost:8080/tasks/${taskId}`, {
         method: 'delete',
     })
-        // .then(async res => await res.json())
         .then(() => {
-            getAllTasks();
+            renderTasks(currentPage);
         })
         .catch(err => {
             console.log("Error: ");
@@ -73,13 +88,15 @@ async function deleteTask(taskId) {
         });
 }
 
-async function saveTask(title, description) {
-    const taskJson = JSON.stringify({title: title.value, description: description.value});
+async function saveTask() {
+    const taskTitle = document.getElementById("title");
+    const description = document.getElementById("description");
+    const taskJson = JSON.stringify({title: taskTitle.value, description: description.value});
 
     await fetch("http://localhost:8080/tasks", {
         method: 'post',
         body: taskJson,
-        headers : {"Content-Type": "application/json"}
+        headers: {"Content-Type": "application/json"}
     })
         .then(async res => await res.json())
         .then(res => {
